d73316352fa98c43da2e5184eab4cf3d3d76968c
Kunal Nandwana <kunal.nandwana@dataeaze.io>
2023-10-29 18:44:19 +0530

    Revert "initial commit"
    This reverts commit d13fa49afd6a80c4a8c21c5e952b78be8515aca6.

--- src/main/scala/common/Constants.scala
+++
@@ -1,9 +0,0 @@
-package common
-
-object Constants {
-
-  val gcpConfigKeyPath:String="/home/kunal/Downloads/agriculture-data-analytics-cbc9c21e274f.json"
-
-  val linkList:List[String]=List("gs://agriculture_data_analytics/landingZone/2023-09-05/*/*.csv","gs://agriculture_data_analytics/landingZone/2023-09-25/*/*.csv","gs://agriculture_data_analytics/landingZone/2023-09-26/*/*.csv","gs://agriculture_data_analytics/landingZone/2023-10-04/*/*.csv","gs://agriculture_data_analytics/landingZone/2023-10-10/*/*.csv","gs://agriculture_data_analytics/landingZone/2023-10-11/*/*.csv","gs://agriculture_data_analytics/landingZone/2023-10-18/*/*.csv")
-
-}
